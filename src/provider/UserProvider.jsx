be00943971fc6d237eeb5dedb41ea49419df39da
Luiz Fernando C. Modolo <modolo.luizfernando@gmail.com>
2021-12-15 19:54:31 -0300

    ajuste arquivo provider

--- src/provider/UserProvider.jsx
+++ src/provider/UserProvider.jsx
@@ -32,7 +32,6 @@ function UserProvider({ children }) {
     } else {
       global.alert('Sinto muito, não encontramos nenhuma receita para esses filtros.');
     }
-    fetchRandonRecipe()
   }
 
   async function fetchCategoryAPI(adressParameter = 'themealdb') {
@@ -48,13 +47,6 @@ function UserProvider({ children }) {
     }
   }
 
-  const fetchRandonRecipe = async () => {
-    const response = await fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php');
-    const result = await response.json();
-    console.log(result);
-    // history.push(`/bebidas/${result.drinks[0].idDrink}`);
-  };
-
   const ContextObj = {
     apiResult,
     setApiResult,
@@ -66,7 +58,6 @@ function UserProvider({ children }) {
     fetchCategoryAPI,
     categorySelected,
     setCategorySelected,
-    fetchRandonRecipe,
   };
 
   return (
